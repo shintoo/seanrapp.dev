9b81af5f7dab0fb7568a80fcf0da3d129eac9277
shintoo <seanprapp@gmail.com>
2025-01-09 22:47:56 -0500

    redux

--- static/js/main.js
+++ static/js/main.js
@@ -9,3 +9,16 @@ const closeModal = (modalId) => {
   modal.style.display = "none";
   document.body.classList.remove("modal-backdrop")
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const idx = new Date().getDay()
+  document.getElementById("sotd").innerHTML = [
+    "young love - lyrical school",
+    "live my life - aespa",
+    "landscape with a fairy - aspidistrafly",
+    "how sweet - newjeans",
+    "prismatism - gregory and the hawk",
+    "blazing in the dark - gnb chili",
+    "海と花束 - きのこ帝国"
+  ][idx]
+})
