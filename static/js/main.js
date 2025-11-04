5da3440989485b979691cfce5ee4140912c6c956
shintoo <seanprapp@gmail.com>
2025-11-04 17:07:06 -0500

    small update

--- static/js/main.js
+++ static/js/main.js
@@ -12,13 +12,15 @@ const closeModal = (modalId) => {
 
 document.addEventListener("DOMContentLoaded", () => {
   const idx = new Date().getDay()
-  document.getElementById("sotd").innerHTML = [
-    "im the visual - yoo mi",
-    "kill ma bo$$ - kiiras",
-    "jellyous - illit",
-    "baddie - ive",
-    "zero - newjeans",
-    "btg - kiiikiii",
-    "shiwa - buzzg"
+  const song = [
+    {"title": "im the visual - yoo mi", "url":"https://youtu.be/p08l5f3oOSY"},
+    { "title": "baddie - ive", "url": "https://youtu.be/Da4P2uT4mVc" },
+    { "title": "nuuamm - maho", "url": "https://youtu.be/l5aEgNjU8xQ" },
+    { "title": "真昼の月 - noisycell", "url": "https://youtu.be/VTKeRmd90NY" },
+    { "title": "cure - honeydip", "url": "https://youtu.be/CljBDaSOr3g" },
+    { "title": "btg - kiiikiii", "url": "https://youtu.be/hnCd1RyTaOA" },
+    { "title": "life in these islands - kawika kahiapo", "url": "https://youtu.be/ps_8HFp3PyI" },
   ][idx]
+
+  document.getElementById("sotd").innerHTML = `<a style="color: white" href="${song.url}" target="_blank">${song.title}</a>`
 })
